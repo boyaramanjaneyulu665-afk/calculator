b4d7554e90bbc09069f09cd36f1b887c785bfc54
boyaramanjaneyulu665-afk <boyaramanjaneyulu665@gmail.com>
2025-11-18 17:18:54 +0530

    this is new commit

---
+++ src/com/calculator/CalciImplementation.java
@@ -0,0 +1,23 @@
+package com.calculator;
+
+public class CalciImplementation implements Calci {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
